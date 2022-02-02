8f73b0438eeae23d4156211776efdb88750e3239
ballchampgap <98090051+ballchampgap@users.noreply.github.com>
2022-02-02 22:12:58 +0700

    aa

--- frminsert.php
+++ frminsert.php
@@ -1,8 +1,8 @@
 <script src="https://code.jquery.com/jquery-3.6.0.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 <?php
-require 'connect.php';
 
+require_once 'connect.php';
 
 $pname = $_POST['pname'];
 $pest_epic = $_POST['pest_epic_id'];
@@ -10,20 +10,29 @@ $planteco = $_POST['planteco_id'];
 $data_pest_epic = $_POST['data_pest_epic_id'];
 $lat = $_POST['lat'];
 $lon = $_POST['lon'];
+// query planteco name_th
+$plant = mysqli_query($conn, "SELECT * FROM planteco WHERE id = '$planteco'");
+while ($row = $plant->fetch_assoc()) {
+    $planteco_name_th = $row['name_th'];
+}
+// query data_pest_epic name_th
+$data_pest_epic_a = mysqli_query($conn, "SELECT * FROM data_pest_epic WHERE id = '$data_pest_epic'");
+while ($row = $data_pest_epic_a->fetch_assoc()) {
+    $data_pest_epic_name_th = $row['name_th'];
+}
 
-if ($pest_epic==1){
-    $sql =  "INSERT INTO epidemic (yname,plant_type,data_epidemic,lat,lon)
-    VALUE ('$pname', '$planteco', '$data_pest_epic','$lat','$lon')";
+if ($pest_epic == 1) {
+    $sql = "INSERT INTO epidemic (yname,plant_type,data_epidemic,lat,lon)
+    VALUE ('$pname', '$planteco_name_th', '$data_pest_epic_name_th','$lat','$lon')";
+    $resultInsert = mysqli_query($conn, $sql);
+} else {
+    $sql = "INSERT INTO pest (yname,plant_type,data_pest,lat,lon)
+    VALUE ('$pname', '$planteco_name_th', '$data_pest_epic_name_th', '$lat','$lon')";
     $resultInsert = mysqli_query($conn, $sql);
 }
-else{
-    $sql =  "INSERT INTO pest (yname,plant_type,data_pest,lat,lon)
-    VALUE ('$pname', '$planteco', '$data_pest_epic', '$lat','$lon')";
-    $resultInsert = mysqli_query($conn, $sql);
-}
-//บันทึกสำเร็จแจ้งเตือนและกระโดดกลับไปหน้าฟอร์ม   
-if ($resultInsert) 
-{echo "<script>
+//บันทึกสำเร็จแจ้งเตือนและกระโดดกลับไปหน้าฟอร์ม
+if ($resultInsert) {
+    echo "<script>
                 $(document).ready(function() {
                     Swal.fire({
                         title: 'success',
@@ -34,7 +43,7 @@ if ($resultInsert)
                     });
                 })
             </script>";
-    header("refresh:2; url=index.php");
+    header('refresh:2; url=index.php');
 }
 
 ?>
--- index.php
+++ index.php
@@ -12,9 +12,13 @@ $query = mysqli_query($conn, $sql);
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Insert Data</title>
     <link href="assets/bootstrap.min.css" rel="stylesheet">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
 </head>
 
 <body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
     <div class="container my-5">
         <div class="card">
             <div class="card-body">
