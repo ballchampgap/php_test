9551a70b599f32f63365c4e7a1f9a4ff48b5b0db
ballchampgap <98090051+ballchampgap@users.noreply.github.com>
2022-02-03 00:47:51 +0700

    ffqq

--- connect.php
+++ connect.php
@@ -1,9 +1,9 @@
 <?php
 
-    $host = "localhost";
-    $username = "root";
-    $password = "";
-    $db_name = "project";
+    $host = "us-cdbr-east-05.cleardb.net";
+    $username = "b0c48520a43ab3";
+    $password = "4e334c79";
+    $db_name = "heroku_1bd642d1abf5180";
 
     $conn = mysqli_connect($host, $username, $password, $db_name); //เชื่อมต่อกับฐานข้อมูล
 
