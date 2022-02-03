9efbde49876ddf9d8a25e39e138157a14a5474be
ballchampgap <98090051+ballchampgap@users.noreply.github.com>
2022-02-04 00:35:07 +0700

    dds]

--- connect.php
+++ connect.php
@@ -8,7 +8,9 @@
 
 
     $conn = mysqli_connect($host, $username, $password, $db_name); //เชื่อมต่อกับฐานข้อมูล
-    mysqli_set_charset($conn, "utf8");
+    mysql_query("SET character_set_results=utf8");
+    mysql_query("SET character_set_client=utf8");
+    mysql_query("SET character_set_connection=utf8");
     if (mysqli_connect_errno())
   {
   echo "Failed to connect to MySQL: " . mysqli_connect_error();
