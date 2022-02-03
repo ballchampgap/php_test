9f6903ba23c64d7b205739836ddfdb63416b1bf3
ballchampgap <98090051+ballchampgap@users.noreply.github.com>
2022-02-04 03:16:12 +0700

    dd7oo7

--- assets/script.js
+++ assets/script.js
@@ -31,7 +31,7 @@ window.onload = function() {
     function runApp() {
         liff.getProfile().then(profile => {
             document.getElementById("pictureUrl").src = profile.pictureUrl;
-            document.getElementsByName("pname").value = profile.displayName;
+            document.getElementByName("pname").value = profile.displayName;
         }).catch(err => console.error(err));
     }
     liff.init({ liffId: "1656861185-pBP33VqE" }, () => {
