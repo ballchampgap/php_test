fe7af349fd1098b049dfb5164bf0a16b9f26df27
ballchampgap <98090051+ballchampgap@users.noreply.github.com>
2022-02-04 02:45:32 +0700

    dds7

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Insert Data</title>
+    <script src="https://static.line-scdn.net/liff/edge/2.1/liff.js"></script>
     <link href="assets/bootstrap.min.css" rel="stylesheet">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
