a7595ad566d202cb8b49736726716aaa476513e0
ballchampgap <98090051+ballchampgap@users.noreply.github.com>
2022-02-10 07:41:19 +0700

    ijhn

--- index.php
+++ index.php
@@ -31,7 +31,7 @@ $query = mysqli_query($conn, $sql);
   background-size: 100%;
 }
 </style>
-<body>
+<body class="bg-primary">
 <div id="login">
     <div class="loading"></div>
     <div class="container my-5">
