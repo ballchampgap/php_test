57090af01ad9e0315448bb48794d063260b8df98
ballchampgap <98090051+ballchampgap@users.noreply.github.com>
2022-02-14 03:16:33 +0700

    ubeg

--- connect.php
+++ connect.php
@@ -1,9 +1,9 @@
 <?php
 	
   $host = "us-cdbr-east-05.cleardb.net";
-  $username = "ba7e9edf71804c";
-  $password = "2c556d4e";
-  $db_name = "heroku_208c777a9db0e9f";
+  $username = "b67d472a787082";
+  $password = "4c1b833f";
+  $db_name = "heroku_4d02897cb09a476";
 
     $conn = mysqli_connect($host, $username, $password, $db_name); //เชื่อมต่อกับฐานข้อมูล
     mysqli_set_charset($conn, "utf8");
