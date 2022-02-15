b0b6677e384998e9036f20beaf555283dab91d4e
ballchampgap <98090051+ballchampgap@users.noreply.github.com>
2022-02-15 11:24:36 +0700

    swerg

--- frminsert.php
+++ frminsert.php
@@ -31,7 +31,7 @@ function getAddress($latitude, $longitude)
         // send http request
         $geocode = file_get_contents($url);
         $json = json_decode($geocode);
-        $address = $json->results[3]->formatted_address;
+        $address = $json->results[6]->formatted_address;
         return $address;
 }
 
