de6cc9db2988dd18fd7983fc29a95a09be3a03f0
ballchampgap <98090051+ballchampgap@users.noreply.github.com>
2022-02-25 22:27:28 +0700

    kjerf

--- index.php
+++ index.php
@@ -110,7 +110,7 @@ $query = mysqli_query($conn, $sql);
                         <input type="hidden" name="lat">
                         <input type="hidden" name="lon">
                         <input type="hidden" name="pname">
-                        <button id="btn" type="submit" class="login100-form-btn">Save</button>
+                        <button id="btn" type="submit" class="btn btn-primary mt-3">Save</button>
                     </div>
             </form>
         </div>
